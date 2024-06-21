b6208c3773813801bdbca32b01e3ac45d0b28db1
zhli <cofd.li@qq.com>
2024-06-21 11:05:04 +0800

    支持major版本号

--- release.sh
+++ release.sh
@@ -1,22 +1,56 @@
 #!/bin/bash
 
+version_add() {
+    # 提取标签中的最后一组数字
+    tag_number=$(echo "$1" | grep -oE '[0-9]+$')
+
+    # 提取标签的前缀（包含可能的数字）
+    prefix=$(echo "$1" | sed -E "s/$tag_number\$//")
+
+    # 将数字部分加1
+    new_tag_number=$((tag_number + 1))
+
+    # 构造新的标签
+    echo "${prefix}${new_tag_number}"
+}
+
 # 获取当前最新的标签
-latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
 
-# 提取标签中的最后一组数字
-tag_number=$(echo "$latest_tag" | grep -oE '[0-9]+$')
+# 检查是否存在标签
+if [ -z "$latest_tag" ]; then
+    new_tag="0.1"
+else
+    echo "当前版本号：${latest_tag}\n请选择一个操作："
+    options=("版本号+1" "小版本号+1" "大版本号+1" "退出")
 
-# 提取标签的前缀（包含可能的数字）
-prefix=$(echo "$latest_tag" | sed -E "s/$tag_number\$//")
-
-# 将数字部分加1
-new_tag_number=$((tag_number + 1))
-
-# 构造新的标签
-new_tag="${prefix}${new_tag_number}"
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "版本号+1")
+                new_tag=$(version_add "$latest_tag")
+                break                
+                ;;
+            "小版本号+1")
+                new_tag="${latest_tag}.1"
+                break
+                ;;
+            "大版本号+1")
+                major_version=$(echo "$latest_tag" | sed 's/\.[0-9]*$//')              
+                new_tag=$(version_add "$major_version")
+                break
+                ;;
+            "退出")
+                echo "退出程序"
+                exit
+                ;;
+            *) echo "无效的选项 $REPLY";;
+        esac
+    done    
+fi
 
 # 提示用户确认
-read -p "新标签：${new_tag}，是否确认创建并推送新标签？ (y/n): " confirm
+read -n 1 -p "新标签：${new_tag}，是否确认创建并推送新标签？ (y/n): " confirm
 
 if [ "$confirm" == "y" ]; then
     # 打印新标签并加到仓库
